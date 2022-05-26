9fec46661e1e2afe2d010041e183db563b2f6571
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-05-27 00:47:00 +0500

    commit

--- chikkiconf/urls.py
+++ chikkiconf/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('api.urls'))
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
