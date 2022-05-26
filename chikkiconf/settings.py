5c6e41eb59616e3a50a6f9b5edb026dfa226a995
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-05-27 00:39:08 +0500

    commit

--- chikkiconf/settings.py
+++ chikkiconf/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ['/proj/static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = "/media/"
