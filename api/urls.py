794c0a267b2642eca93851a9f53b4bceef7b77e0
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-05-27 00:04:54 +0500

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.urls import path
 
 from accaunt.views import register, register_accepted
+from chikkiconf import settings
 
 urlpatterns = [
     path('register/', register),
     path('register_accepted/', register_accepted),
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
