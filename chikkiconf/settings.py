6c8ef14c43a0cf67fd871e14e692c3227f0ee6ce
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-05-27 00:32:59 +0500

    commit

--- chikkiconf/settings.py
+++ chikkiconf/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATIC_ROOT = [BASE_DIR / "static"]
+# STATICFILES_DIRS = ['/proj/static/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 
 MEDIA_URL = "/media/"
