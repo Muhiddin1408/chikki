eee7acaa1251f15b00e0455188f100c8582fb232
Muhiddin1408 <81641677+Muhiddin1408@users.noreply.github.com>
2022-05-30 14:15:13 +0500

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls.static import static
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from accaunt.views import *
 from chikkiconf import settings
+#
+router = DefaultRouter()
+router.register('home', Home)
+
 
 urlpatterns = [
     path('register/', register),
     path('register_accepted/', register_accepted),
-    # path('', ),
+    path('', include(router.urls)),
+
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
